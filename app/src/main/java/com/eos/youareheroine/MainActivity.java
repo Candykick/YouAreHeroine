9a0dc05fb1fe62aa92379aa28f6f8c3697a03a54
2015000346 <woo171tm@naver.com>
2020-10-05 23:02:37 +0900

    update

--- app/src/main/java/com/eos/youareheroine/MainActivity.java
+++ app/src/main/java/com/eos/youareheroine/MainActivity.java
@@ -2,7 +2,12 @@ package com.eos.youareheroine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
