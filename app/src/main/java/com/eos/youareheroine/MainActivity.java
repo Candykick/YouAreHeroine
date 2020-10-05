a184cf6f8664aa03eec3efe9751989e29b020eb2
LeeGaeun <grace915@naver.com>
2020-10-06 01:47:17 +0900

    login

--- app/src/main/java/com/eos/youareheroine/MainActivity.java
+++ app/src/main/java/com/eos/youareheroine/MainActivity.java
@@ -1,10 +1,31 @@
 package com.eos.youareheroine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton latest;
+    private ImageButton writer;
+    private ImageButton search;
+    private ImageButton king;
+    private ImageButton all;
+    private ImageButton made;
+    private ImageButton write;
+
+    private Toolbar toolbar;
+    private ImageButton main;
+    private ImageButton mypage;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +34,30 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        latest = findViewById(R.id.main_ibtn_latest);
+        writer = findViewById(R.id.main_ibtn_writer);
+        search = findViewById(R.id.main_ibtn_search);
+        king = findViewById(R.id.main_ibtn_king);
+        all = findViewById(R.id.main_ibtn_all);
+        made = findViewById(R.id.main_ibtn_made);
+        write = findViewById(R.id.main_ibtn_write);
+
+        main = findViewById(R.id.ibtn_main);
+        mypage = findViewById(R.id.ibtn_mypage);
+
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }
+
