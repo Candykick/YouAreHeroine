afee576478c617c23ea5a8d8989fcb87e82066d1
2019055223 <2019055223@hanyang.ac.kr>
2020-10-05 23:34:37 +0900

    test

---
+++ app/src/main/java/com/eos/youareheroine/WriterPageActivity.java
@@ -0,0 +1,14 @@
+package com.eos.youareheroine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writer_page);
+    }
+}
